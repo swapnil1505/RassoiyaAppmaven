64bd3e098a020130a6bf7aa5180fac9820614cbb
Manish <manishksingh1991@gmail.com>
2020-07-29 21:00:48 +0530

    #173873092 Auth 2.0 or  JWT integration As a normal user/Admin Login screen with valid user & Password Part 2 .

--- src/main/java/com/rasoiyya/config/WebSecurityConfiguration.java
+++ src/main/java/com/rasoiyya/config/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.rasoiyya.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,20 +14,37 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+import io.github.jhipster.config.JHipsterProperties;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@Import(SecurityProblemSupport.class)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
     @Autowired
     private UserDetailsService userDetailsService;
 
+    private final SecurityProblemSupport problemSupport;
+
+    public WebSecurityConfiguration(SecurityProblemSupport problemSupport) {
+        this.problemSupport = problemSupport;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 
-        .and().authorizeRequests().antMatchers("/oauth/token")
-        .permitAll().anyRequest().authenticated();
+		http.csrf().disable().exceptionHandling().authenticationEntryPoint(problemSupport)
+				.accessDeniedHandler(problemSupport).and().headers()
+				.contentSecurityPolicy(
+						"default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
+				.and().referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN).and()
+				.featurePolicy(
+						"geolocation 'none'; midi 'none'; sync-xhr 'none'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; fullscreen 'self'; payment 'none'")
+				.and().frameOptions().deny().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/oauth/token").permitAll().anyRequest().authenticated();
     }
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
