e3cd31fc9f91b43b93c328c48d363d009b72804a
ravi <ravikantshakya2011@gmail.com>
2020-08-15 14:03:58 +0530

    change template id

--- src/main/java/com/rasoiyya/constants/MessageConstants.java
+++ src/main/java/com/rasoiyya/constants/MessageConstants.java
@@ -10,7 +10,7 @@ public class MessageConstants {
 	public static final String POST_URL = "https://www.fast2sms.com/dev/bulk";
 	public static final String AUTH_KEY = "Fm4Vt2UHbi7nOMjzcAhZCaDdw3ux8KGYJpgNkP9RloQ6XrsyTqr51KG3SR6qYomAJHOvaud4thUlsegy";
 	public static final String OTP_VARIBLE_LEN = "{BB}";
-	public static final String MESSAGE_TEMPLATE_ID = "33090";
+	public static final String MESSAGE_TEMPLATE_ID = "33250";
 	public static final String SENDER_ID = "FSTSMS";
 
 	public MessageConstants() {
