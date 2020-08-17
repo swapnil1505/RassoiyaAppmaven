98e44ded7e63f89ce0bf2cdd857eb26cc2ae60de
suraj singh <surajtech003@gmail.com>
2020-08-17 21:16:45 +0530

    #174133034 : added reviewed changes code

--- src/main/java/com/rasoiyya/domain/Customer.java
+++ src/main/java/com/rasoiyya/domain/Customer.java
@@ -19,10 +19,30 @@ public class Customer extends Auditable implements Serializable {
 	@Column(name="customer_id")
 	private int customerId;
 
+	//bi-directional many-to-one association to BookingDetail
+//	@OneToMany(mappedBy="customer")
+//	private List<BookingDetail> bookingDetails;
+
 	//bi-directional many-to-one association to Person
 	@ManyToOne
 	@JoinColumn(name="person_id")
 	private Person person;
+
+	//bi-directional many-to-one association to Feedback
+//	@OneToMany(mappedBy="customer")
+//	private List<Feedback> feedbacks;
+
+	//bi-directional many-to-one association to Invoice
+//	@OneToMany(mappedBy="customer")
+//	private List<Invoice> invoices;
+
+	//bi-directional many-to-one association to Payment
+//	@OneToMany(mappedBy="customer")
+//	private List<Payment> payments;
+
+	//bi-directional many-to-one association to Wallet
+//	@OneToMany(mappedBy="customer")
+//	private List<Wallet> wallets;
 
 	public Customer() {
 	}
@@ -35,6 +55,28 @@ public class Customer extends Auditable implements Serializable {
 		this.customerId = customerId;
 	}
 
+//	public List<BookingDetail> getBookingDetails() {
+//		return this.bookingDetails;
+//	}
+//
+//	public void setBookingDetails(List<BookingDetail> bookingDetails) {
+//		this.bookingDetails = bookingDetails;
+//	}
+//
+//	public BookingDetail addBookingDetail(BookingDetail bookingDetail) {
+//		getBookingDetails().add(bookingDetail);
+//		bookingDetail.setCustomer(this);
+//
+//		return bookingDetail;
+//	}
+//
+//	public BookingDetail removeBookingDetail(BookingDetail bookingDetail) {
+//		getBookingDetails().remove(bookingDetail);
+//		bookingDetail.setCustomer(null);
+//
+//		return bookingDetail;
+//	}
+
 	public Person getPerson() {
 		return this.person;
 	}
@@ -42,5 +84,93 @@ public class Customer extends Auditable implements Serializable {
 	public void setPerson(Person person) {
 		this.person = person;
 	}
+
+//	public List<Feedback> getFeedbacks() {
+//		return this.feedbacks;
+//	}
+//
+//	public void setFeedbacks(List<Feedback> feedbacks) {
+//		this.feedbacks = feedbacks;
+//	}
+//
+//	public Feedback addFeedback(Feedback feedback) {
+//		getFeedbacks().add(feedback);
+//		feedback.setCustomer(this);
+//
+//		return feedback;
+//	}
+//
+//	public Feedback removeFeedback(Feedback feedback) {
+//		getFeedbacks().remove(feedback);
+//		feedback.setCustomer(null);
+//
+//		return feedback;
+//	}
+//
+//	public List<Invoice> getInvoices() {
+//		return this.invoices;
+//	}
+//
+//	public void setInvoices(List<Invoice> invoices) {
+//		this.invoices = invoices;
+//	}
+//
+//	public Invoice addInvoice(Invoice invoice) {
+//		getInvoices().add(invoice);
+//		invoice.setCustomer(this);
+//
+//		return invoice;
+//	}
+//
+//	public Invoice removeInvoice(Invoice invoice) {
+//		getInvoices().remove(invoice);
+//		invoice.setCustomer(null);
+//
+//		return invoice;
+//	}
+//
+//	public List<Payment> getPayments() {
+//		return this.payments;
+//	}
+//
+//	public void setPayments(List<Payment> payments) {
+//		this.payments = payments;
+//	}
+//
+//	public Payment addPayment(Payment payment) {
+//		getPayments().add(payment);
+//		payment.setCustomer(this);
+//
+//		return payment;
+//	}
+//
+//	public Payment removePayment(Payment payment) {
+//		getPayments().remove(payment);
+//		payment.setCustomer(null);
+//
+//		return payment;
+//	}
+//
+//	public List<Wallet> getWallets() {
+//		return this.wallets;
+//	}
+//
+//	public void setWallets(List<Wallet> wallets) {
+//		this.wallets = wallets;
+//	}
+//
+//	public Wallet addWallet(Wallet wallet) {
+//		getWallets().add(wallet);
+//		wallet.setCustomer(this);
+//
+//		return wallet;
+//	}
+//
+//	public Wallet removeWallet(Wallet wallet) {
+//		getWallets().remove(wallet);
+//		wallet.setCustomer(null);
+//
+//		return wallet;
+//	}
 
 }
--- src/main/java/com/rasoiyya/service/customer/impl/CustomerRegistrationServiceImpl.java
+++ src/main/java/com/rasoiyya/service/customer/impl/CustomerRegistrationServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rasoiyya.domain.Address;
 import com.rasoiyya.domain.City;
@@ -61,6 +62,7 @@ public class CustomerRegistrationServiceImpl implements CustomerRegistrationServ
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
+	@Transactional
 	@Override
 	public void createCustomerRegistration(CustomerRegistrationRequest customerRegistrationRequest) throws Exception {
 		try {
